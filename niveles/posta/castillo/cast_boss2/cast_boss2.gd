93bef54bdb3c38fc2ac091c3c17e615f9c2f5923
Manuel Avalos <78101820+Manuvalos@users.noreply.github.com>
2022-06-27 15:46:04 -0300

    -
    -

--- niveles/posta/castillo/cast_boss2/cast_boss2.gd
+++ niveles/posta/castillo/cast_boss2/cast_boss2.gd
@@ -5,6 +5,7 @@ const NOMBRE_DATO := "boss_muerto"
 export(NodePath) onready var limites_camara_boss = get_node(limites_camara_boss) as CameraBounds
 export(NodePath) onready var puerta = get_node(puerta) as Puerta
 export(NodePath) onready var trigger_empezar_boss = get_node(trigger_empezar_boss) as TriggerOnce
+export(NodePath) onready var barra_hp = get_node(barra_hp) as BarraHPBoss
 
 func _ready() -> void:
 	if !info_persist_nivel.has(NOMBRE_DATO):
